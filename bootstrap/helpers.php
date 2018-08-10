c38c9d12735a3889717c6e573fdf63c17aecfa40
亮亮 <1095036273@qq.com>
2018-08-10 17:00:41 +0000

    辅助函数

---
+++ bootstrap/helpers.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @Author: name
+ * @Date:   2018-08-11 00:55:52
+ * @Last Modified by:   name
+ * @Last Modified time: 2018-08-11 00:56:03
+ */
+function test_helper() {
+    return 'OK';
+}
