0e071ef6d012ce1f0b350b7302f9864e75e21fd1
亮亮 <1095036273@qq.com>
2018-08-11 07:18:22 +0000

    触发激活邮件

--- routes/web.php
+++ routes/web.php
@@ -9,21 +9,19 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::group(['middleware' => 'auth'], function() {
+Route::group(['middleware' => 'auth'], function () {
     Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-
+    Route::get('/email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
+    Route::get('/email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
     // 开始
-    Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
-        Route::get('/test1', 'PagesController@test')->name('test1');
+    Route::group(['middleware' => 'email_verified'], function () {
+
     });
     // 结束
 });
