507fbc6d425e577cfd927f374deae9849f06cc06
亮亮 <1095036273@qq.com>
2018-10-21 09:36:49 +0000

    用户界面 - 订单列表页

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -65,6 +65,11 @@
                             </a>
                         </li>
                         <li>
+                            <a href="{{ route('orders.index') }}">
+                                我的订单
+                            </a>
+                        </li>
+                        <li>
                             <a href="{{ route('products.favorites') }}">
                                 我的收藏
                             </a>
