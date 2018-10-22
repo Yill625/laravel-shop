5cc7989aa7ebc5da05faad9e4076d3d9cdfca3af
vagrant <vagrant@homestead>
2018-10-22 08:50:21 +0000

    订单支付

--- database/admin.sql
+++ database/admin.sql
@@ -19,80 +19,80 @@
 -- Dumping data for table `admin_menu`
 --
 
-LOCK TABLES `admin_menu` WRITE;
+
 /*!40000 ALTER TABLE `admin_menu` DISABLE KEYS */;
 INSERT INTO `admin_menu` VALUES (1,0,1,'首页','fa-bar-chart','/',NULL,'2018-08-13 20:53:55'),(2,0,2,'系统管理','fa-tasks',NULL,NULL,'2018-08-13 20:54:15'),(3,2,3,'管理员','fa-users','auth/users',NULL,'2018-08-13 20:54:34'),(4,2,4,'角色','fa-user','auth/roles',NULL,'2018-08-13 20:54:55'),(5,2,5,'权限','fa-ban','auth/permissions',NULL,'2018-08-13 20:55:07'),(6,2,6,'菜单','fa-bars','auth/menu',NULL,'2018-08-13 20:55:21'),(7,2,7,'操作日志','fa-history','auth/logs',NULL,'2018-08-13 20:55:35'),(8,0,8,'用户管理','fa-users','/users','2018-08-13 21:06:00','2018-08-13 21:07:29'),(9,0,0,'商品管理','fa-cubes','/products','2018-08-15 16:57:57','2018-08-15 16:57:57');
 /*!40000 ALTER TABLE `admin_menu` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_permissions`
 --
 
-LOCK TABLES `admin_permissions` WRITE;
+
 /*!40000 ALTER TABLE `admin_permissions` DISABLE KEYS */;
 INSERT INTO `admin_permissions` VALUES (1,'All permission','*','','*',NULL,NULL),(2,'Dashboard','dashboard','GET','/',NULL,NULL),(3,'Login','auth.login','','/auth/login\r\n/auth/logout',NULL,NULL),(4,'User setting','auth.setting','GET,PUT','/auth/setting',NULL,NULL),(5,'Auth management','auth.management','','/auth/roles\r\n/auth/permissions\r\n/auth/menu\r\n/auth/logs',NULL,NULL),(6,'用户管理','users','','/users*','2018-08-13 21:31:51','2018-08-13 21:31:51');
 /*!40000 ALTER TABLE `admin_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_role_menu`
 --
 
-LOCK TABLES `admin_role_menu` WRITE;
+
 /*!40000 ALTER TABLE `admin_role_menu` DISABLE KEYS */;
 INSERT INTO `admin_role_menu` VALUES (1,2,NULL,NULL);
 /*!40000 ALTER TABLE `admin_role_menu` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_role_permissions`
 --
 
-LOCK TABLES `admin_role_permissions` WRITE;
+
 /*!40000 ALTER TABLE `admin_role_permissions` DISABLE KEYS */;
 INSERT INTO `admin_role_permissions` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL),(2,3,NULL,NULL),(2,4,NULL,NULL),(2,6,NULL,NULL);
 /*!40000 ALTER TABLE `admin_role_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_role_users`
 --
 
-LOCK TABLES `admin_role_users` WRITE;
+
 /*!40000 ALTER TABLE `admin_role_users` DISABLE KEYS */;
 INSERT INTO `admin_role_users` VALUES (1,1,NULL,NULL),(2,2,NULL,NULL);
 /*!40000 ALTER TABLE `admin_role_users` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_roles`
 --
 
-LOCK TABLES `admin_roles` WRITE;
+
 /*!40000 ALTER TABLE `admin_roles` DISABLE KEYS */;
 INSERT INTO `admin_roles` VALUES (1,'Administrator','administrator','2018-08-12 16:34:53','2018-08-12 16:34:53'),(2,'运营','operator','2018-08-13 21:34:27','2018-08-13 21:34:27');
 /*!40000 ALTER TABLE `admin_roles` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_user_permissions`
 --
 
-LOCK TABLES `admin_user_permissions` WRITE;
+
 /*!40000 ALTER TABLE `admin_user_permissions` DISABLE KEYS */;
 /*!40000 ALTER TABLE `admin_user_permissions` ENABLE KEYS */;
-UNLOCK TABLES;
+
 
 --
 -- Dumping data for table `admin_users`
 --
 
-LOCK TABLES `admin_users` WRITE;
+
 /*!40000 ALTER TABLE `admin_users` DISABLE KEYS */;
 INSERT INTO `admin_users` VALUES (1,'admin','$2y$10$AlzG865JmH4uoaaip0y6EuUeRKm.DZgIb/oJnhW/f/ir3h9SfEyOi','Administrator',NULL,'wVxJmlssZmTAoNMXPsPDYONJwb8uCbmrJ3Vy0lGmEbAAWAdNozrwgqyWeJMy','2018-08-12 16:34:53','2018-08-12 16:34:53'),(2,'operator','$2y$10$c929e6Gu.2Xol.OE044CzOJ5BmPyk4jEUhpgRBiAMxmagRV1skOfy','运营',NULL,NULL,'2018-08-13 21:44:15','2018-08-13 21:44:15');
 /*!40000 ALTER TABLE `admin_users` ENABLE KEYS */;
-UNLOCK TABLES;
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
