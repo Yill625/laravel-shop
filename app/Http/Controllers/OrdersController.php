5b646c46859e421d934b5f476028cfdf568c2639
vagrant <vagrant@homestead>
2018-10-24 03:57:32 +0000

    评价商品

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -12,13 +12,15 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Services\CartService;
 use App\Services\OrderService;
+use App\Http\Requests\SendReviewRequest;
+use App\Events\OrderReviewed;
 
 class OrdersController extends Controller
 {
     public function store(OrderRequest $request, OrderService $orderService)
     {
-        $user    = $request->user();
-        
+        $user = $request->user();
+
         $address = UserAddress::find($request->input('address_id'));
 
         return $orderService->store($user, $address, $request->input('remark'), $request->input('items'));
@@ -27,7 +29,7 @@ class OrdersController extends Controller
     public function index(Request $request)
     {
         $orders = Order::query()
-        // 使用 with 方法预加载，避免N + 1问题
+            // 使用 with 方法预加载，避免N + 1问题
             ->with(['items.product', 'items.productSku'])
             ->where('user_id', $request->user()->id)
             ->orderBy('created_at', 'desc')
@@ -41,5 +43,50 @@ class OrdersController extends Controller
         $this->authorize('own', $order);
 
         return view('orders.show', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function review(Order $order)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        // 判断是否已经支付
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 使用 load 方法加载关联数据，避免 N + 1 性能问题
+        return view('orders.review', ['order' => $order->load(['items.productSku', 'items.product'])]);
+    }
+
+    public function sendReview(Order $order, SendReviewRequest $request)
+    {
+        // 校验权限
+        $this->authorize('own', $order);
+        if (!$order->paid_at) {
+            throw new InvalidRequestException('该订单未支付，不可评价');
+        }
+        // 判断是否已经评价
+        if ($order->reviewed) {
+            throw new InvalidRequestException('该订单已评价，不可重复提交');
+        }
+        $reviews = $request->input('reviews');
+        // 开启事务
+        \DB::transaction(function () use ($reviews, $order) {
+            // 遍历用户提交的数据
+            foreach ($reviews as $review) {
+                $orderItem = $order->items()->find($review['id']);
+                // 保存评分和评价
+                $orderItem->update([
+                    'rating'      => $review['rating'],
+                    'review'      => $review['review'],
+                    'reviewed_at' => Carbon::now(),
+                ]);
+            }
+            // 将订单标记为已评价
+            $order->update(['reviewed' => true]);
+            // 事件
+            event(new OrderReviewed($order));
+        });
+
+        return redirect()->back();
     }
 }
